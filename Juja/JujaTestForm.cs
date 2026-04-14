f7114aa0acb8ff1e9e42e12cb9623295460a6153
DmtrDv <dmitriybychkov75146@gmail.com>
2026-04-14 18:15:05 +0300

    Добавьте файлы проекта.

---
+++ Juja/JujaTestForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juja
+{
+    public partial class JujaTestForm: Form
+    {
+        // Импортируем функцию для получения глобальных координат курсора
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+
+        // Структура для координат
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        // Смещение относительно курсора (чтобы объект не перекрывал курсор)
+        //private readonly Point offset = new Point(20, 20);
+        Cursor cursor;
+        public JujaTestForm()
+        {
+            InitializeComponent();
+
+            // Настройка формы
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.ShowInTaskbar = true;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Size = new Size(100, 125); // размер "объекта"
+            this.BackColor = Color.Red;      // цвет для наглядности
+
+            // Можно сделать круглую форму через Region (опционально)
+
+            // Таймер для обновления позиции
+            Timer timer = new Timer();
+            timer.Interval = 15; // 15 мс ≈ 66 FPS
+            //timer.Tick += Timer_Tick;
+            timer.Tick += MoveToCursor_Timer_Tick;
+            timer.Start();
+        }
+        private void MoveToCursor_Timer_Tick(object sender, EventArgs e)
+        {
+            GetCursorPos(out POINT cursorPos);
+            //this.Location = new Point();
+            /*if (this.Location.X <= cursorPos.X - 50 || this.Location.X >= cursorPos.X + 50 && this.Location.Y <= cursorPos.Y - 50 || this.Location.Y >= cursorPos.Y + 50)
+            {
+                this.Location = this.Location;
+            }*/
+            /*if (cursorPos.X - 50 <= this.Location.X || cursorPos.X + 50 >= this.Location.X  && cursorPos.Y - 50 <= this.Location.Y || cursorPos.Y + 50 >= this.Location.Y)
+            {
+                this.Location = this.Location;
+            }
+            else
+            {*/
+                int X = cursorPos.X;
+                int Y = cursorPos.Y;
+
+            int NewPlusX = 5;//(X - this.Location.X) / 100; 
+            int NewPlusY = 5; //(Y - this.Location.Y) / 100; 
+
+              /*  if (NewPlusX < 5)
+                {
+                    NewPlusX = 5;
+                }
+                if (NewPlusY < 5)
+                {
+                    NewPlusY = 5;
+                }*/
+
+                if (this.Location.X > cursorPos.X && this.Location.Y > cursorPos.Y)
+                {
+                    this.Location = new Point(this.Location.X - NewPlusX, this.Location.Y - NewPlusY);
+                }
+                else if (this.Location.X < cursorPos.X && this.Location.Y < cursorPos.Y)
+                {
+                    this.Location = new Point(this.Location.X + NewPlusX, this.Location.Y + NewPlusY);
+                }
+                else if (this.Location.X < cursorPos.X && this.Location.Y > cursorPos.Y)
+                {
+                    this.Location = new Point(this.Location.X + NewPlusX, this.Location.Y - NewPlusY);
+                }
+                else
+                {
+                    this.Location = new Point(this.Location.X - NewPlusX, this.Location.Y + NewPlusY);
+                }
+            //}
+
+
+            /*
+            // Получаем глобальную позицию курсора
+            if (GetCursorPos(out POINT cursorPos))
+            {
+                // Устанавливаем позицию окна со смещением
+                //this.Location = new Point(cursorPos.X + offset.X, cursorPos.Y + offset.Y);
+            }*/
+        }
+
+        // Чтобы форма не исчезала при Alt+Tab, можно переопределить CreateParams
+      /*  protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x80; // WS_EX_TOOLWINDOW – не показывать в Alt+Tab
+                return cp;
+            }
+        }*/
+    }
+}
---
+++ Juja/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Juja")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Juja")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8b0bed23-59ea-4a7e-8661-aec691372cfa")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
